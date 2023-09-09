4e4b9ff6b309f2405bc14f11c799b44f1353ca12
Mark (Krittipoom) <122365475+Krittipoom@users.noreply.github.com>
2023-09-09 21:07:51 +0700

    Update firstsetup.sh

--- firstsetup.sh
+++ firstsetup.sh
@@ -1,3 +1,5 @@
+ sudo apt update -y && sudo apt upgrade -y
+
 # movepackage
 mv ./ubuntu-setup/*.deb ./
 mv ./ubuntu-setup/.updatetime ./
@@ -11,10 +13,10 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
 sudo rm microsoft.gpg
-sudo apt update && sudo apt install microsoft-edge-stable
+sudo apt update && sudo apt install Microsoft-edge-stable -y
 
 # git
-sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt update && sudo apt install git -y
 
 # gh
@@ -22,7 +24,7 @@ sudo apt install gh -y
 #gh auth login
 
 # ulauncher
-sudo add-apt-repository ppa:agornostal/ulauncher
+sudo add-apt-repository ppa:agornostal/launcher -y
 sudo apt update
 sudo apt install ulauncher -y
 
@@ -44,18 +46,18 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # docker desktop
-sudo apt install gnome-terminal
+sudo apt install gnome-terminal -y
 sudo apt-get update
 wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.22.1-amd64.deb
 sudo apt-get install ./docker-desktop-4.22.1-amd64.deb -y
 
 # Python3.9 (deadsnake)
 sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update && sudo apt install python3.9 -y
 
 # Python3.9 pip
-sudo apt install python3.9-distutils
+sudo apt install python3.9-distutils -y
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
 
