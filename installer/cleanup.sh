20cb718cd55f56b9511d48b637af6c735a401da2
Krittipoom <krittipoom@outlook.com>
2023-09-15 12:21:06 +0700

    add cleanup to install

---
+++ installer/cleanup.sh
@@ -0,0 +1,3 @@
+sudo apt install python3-apt python3-regex -y
+wget https://github.com/davidfoerster/aptsources-cleanup/releases/download/v0.1.7.5.2/aptsources-cleanup.pyz
+sudo chmod 777 aptsources-cleanup.pyz
