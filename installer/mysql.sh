92912789c28b0a109b6c6299e26c4e0871bce928
Krittipoom <krittipoom@outlook.com>
2023-09-15 11:13:43 +0700

    edit mysql and python version

--- installer/mysql.sh
+++ installer/mysql.sh
@@ -1,1 +1,3 @@
 sudo apt install mysql-server -y
+sudo apt install pkg-config -y
+sudo apt-get install default-libmysqlclient-dev -y
--- installer/python39-deadsnake-and-pip.sh
+++ installer/python39-deadsnake-and-pip.sh
@@ -1,6 +1,7 @@
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update && sudo apt install python3.9 -y
+sudo apt-get install python3.9-dev -y
 
 #pip
 sudo apt install python3.9-distutils -y
