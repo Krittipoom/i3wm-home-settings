84749cfc746cbcef125ced10597bcd31c97f3b62
Krittipoom <krittipoom@outlook.com>
2023-09-19 11:04:11 +0700

    add ms-edge and cleanup.pyz to test

--- .github/workflows/test_script/aptlist.sh
+++ .github/workflows/test_script/aptlist.sh
@@ -1,2 +1,2 @@
-sudo apt list alacarte gh git mysql-client* python3.9 sublime-text ulauncher xapp zoom
+sudo apt list alacarte docker* gh git mysql-client* microsoft-edge* python3.9 sublime-text ulauncher xapp zoom
 ls -al
